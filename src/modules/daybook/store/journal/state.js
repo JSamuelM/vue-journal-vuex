5e8a0bc249dc3aaa0f2bd54799ebd081cc59921f
Samuel Mena <samuel23mena@gmail.com>
2021-10-20 08:03:15 -0600

    :lipstick: Finalizando cambios de UI

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,21 +2,21 @@ export default () => ({
   isLoading: true,
   entries: [
     {
-      id: new Date().getTime(), // 12231312412
+      id: '1', // 12231312412
       date: new Date().toDateString(), // sat 23, june
-      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque expedita, aut consequuntur officia eum vel debitis, eveniet eos quibusdam dolorem facilis architecto repellat molestiae nulla. In nihil architecto dolores accusamus magnam eaque beatae voluptatem, vitae nostrum fuga itaque vero laudantium tempora sapiente libero id accusantium. Doloribus autem voluptatibus ullam omnis.',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque expedita, aut consequuntur officia eum vel debitis, eveniet eos quibusdam dolorem facilis architecto repellat molestiae nulla. In nihil architecto dolores accusamus magnam eaque beatae voluptatem, vitae nostrum fuga itaque vero laudantium tempora sapiente libero id accusantium. Doloribus autem voluptatibus ullam omnis. Suspendisse faucibus fermentum diam, sit amet placerat neque viverra nec. Vestibulum volutpat purus a porta. Donec sapien massa, ultrices in quam non, viverra placerat erat. Mauris ac massa augue. Etiam pharetra ipsum ut sapien cursus, quis tempus ex facilisis.',
       picture: null,
     },
     {
-      id: new Date().getTime() + 1000, // 12231312412
+      id: '2', // 12231312412
       date: new Date().toDateString(), // sat 23, june
-      text: 'Atque soluta in recusandae et dolor corrupti non reprehenderit voluptatum amet iusto sint iste ex velit possimus, ullam laborum eum, repudiandae laudantium? Qui tempore architecto quidem, dolor suscipit modi minima sequi fuga unde dolorum sit exercitationem quibusdam quod accusantium iste?,',
+      text: 'Atque soluta in recusandae et dolor corrupti non reprehenderit voluptatum amet iusto sint iste ex velit possimus, ullam laborum eum, repudiandae laudantium? Qui tempore architecto quidem, dolor suscipit modi minima sequi fuga unde dolorum sit exercitationem quibusdam quod accusantium iste? Donec molestie fermentum mauris, in facilisis ante venenatis eget. Vivamus pulvinar eu libero ac placerat. Etiam imperdiet dui a varius viverra. Vestibulum dapibus erat vitae dolor vestibulum ultrices. Donec sapien massa, ultrices in quam non, viverra placerat erat. Mauris ac massa augue. Etiam pharetra ipsum ut sapien cursus, quis tempus ex facilisis.',
       picture: null,
     },
     {
-      id: new Date().getTime() + 2000, // 12231312412
+      id: '3', // 12231312412
       date: new Date().toDateString(), // sat 23, june
-      text: 'Beatae eligendi molestias ad sint odit aspernatur veritatis libero voluptas vitae sit iure expedita, suscipit eius quasi dolorum ullam porro impedit ex, incidunt maiores. Earum excepturi quisquam laborum necessitatibus saepe exercitationem iure voluptate doloribus doloremque totam, asperiores neque magni. Impedit alias ipsam voluptatibus aspernatur quo amet deserunt, magni commodi voluptatem voluptates voluptate consectetur facere, deleniti quae. Est esse, alias reprehenderit voluptatibus expedita porro iusto, ab, magnam ipsam at quod repudiandae qui nemo unde!',
+      text: 'Beatae eligendi molestias ad sint odit aspernatur veritatis libero voluptas vitae sit iure expedita, suscipit eius quasi dolorum ullam porro impedit ex, incidunt maiores. Earum excepturi quisquam laborum necessitatibus saepe exercitationem iure voluptate doloribus doloremque totam, asperiores neque magni. Impedit alias ipsam voluptatibus aspernatur quo amet deserunt, magni commodi voluptatem voluptates voluptate consectetur facere, deleniti quae. Est esse, alias reprehenderit voluptatibus expedita porro iusto, ab, magnam ipsam at quod repudiandae qui nemo unde! Donec sapien massa, ultrices in quam non, viverra placerat erat. Mauris ac massa augue. Etiam pharetra ipsum ut sapien cursus, quis tempus ex facilisis.',
       picture: null,
     },
   ]
