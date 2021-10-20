a6e9df4f29adfaba4bc705f7040f439aff1656af
Samuel Mena <samuel23mena@gmail.com>
2021-10-19 23:53:58 -0600

    :tada: First commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: new Date().getTime(), // 12231312412
+      date: new Date().toDateString(), // sat 23, june
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque expedita, aut consequuntur officia eum vel debitis, eveniet eos quibusdam dolorem facilis architecto repellat molestiae nulla. In nihil architecto dolores accusamus magnam eaque beatae voluptatem, vitae nostrum fuga itaque vero laudantium tempora sapiente libero id accusantium. Doloribus autem voluptatibus ullam omnis.',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000, // 12231312412
+      date: new Date().toDateString(), // sat 23, june
+      text: 'Atque soluta in recusandae et dolor corrupti non reprehenderit voluptatum amet iusto sint iste ex velit possimus, ullam laborum eum, repudiandae laudantium? Qui tempore architecto quidem, dolor suscipit modi minima sequi fuga unde dolorum sit exercitationem quibusdam quod accusantium iste?,',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000, // 12231312412
+      date: new Date().toDateString(), // sat 23, june
+      text: 'Beatae eligendi molestias ad sint odit aspernatur veritatis libero voluptas vitae sit iure expedita, suscipit eius quasi dolorum ullam porro impedit ex, incidunt maiores. Earum excepturi quisquam laborum necessitatibus saepe exercitationem iure voluptate doloribus doloremque totam, asperiores neque magni. Impedit alias ipsam voluptatibus aspernatur quo amet deserunt, magni commodi voluptatem voluptates voluptate consectetur facere, deleniti quae. Est esse, alias reprehenderit voluptatibus expedita porro iusto, ab, magnam ipsam at quod repudiandae qui nemo unde!',
+      picture: null,
+    },
+  ]
+})
